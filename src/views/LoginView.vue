cdbd27d26c7d6d981021b935114410a5bbf43c05
Lara Hernandes <larachernandes@gmail.com>
2022-04-11 22:59:42 -0300

    feat (inicial commit): created project configuration

---
+++ src/views/LoginView.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+  name: "LoginView",
+};
+</script>
+
+<template>
+  <div class="container mx-auto border">
+    <h3 class="mx-auto border w-52 text-3xl font-bold underline">Login</h3>
+  </div>
+</template>
